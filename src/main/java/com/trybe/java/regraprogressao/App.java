4ee278193f45222fb2a3841e37a364ae4b35d888
Ruan-Portella <ruanmorales29@gmail.com>
2023-10-24 15:42:22 -0300

    Feat: Requisito 01 Concluido

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,42 @@
 package com.trybe.java.regraprogressao;
 
-/**
- * App.
- */
+import java.util.Scanner;
+
 public class App {
 
   /**
-   * Metodo main.
+   * Método main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int quantidadeAtividades = Integer.parseInt(scanner.nextLine());
+    String[] nomesAtividades = new String[quantidadeAtividades];
+    double[] notasAtividades = new double[quantidadeAtividades];
+    double[] pesosAtividades = new double[quantidadeAtividades];
+    double somaPessos = 0;
+    double notaFinal = 0;
+
+    for (int i = 0; i < quantidadeAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      nomesAtividades[i] = scanner.nextLine();
+      System.out.println("Digite a nota da atividade " + (i + 1) + ":");
+      notasAtividades[i] = scanner.nextDouble();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      pesosAtividades[i] = scanner.nextDouble();
+      somaPessos += pesosAtividades[i];
+      notaFinal += (notasAtividades[i] * pesosAtividades[i]) / 100;
+      scanner.nextLine();
+    }
+
+    if (somaPessos != 100) {
+      System.out.println("A soma dos pesos deve ser igual a 100");
+      System.exit(1);
+    }
+
+    System.out.println("A nota final é: " + notaFinal);
+
+    scanner.close(); 
+
   }
 }
